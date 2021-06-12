d9fcafb68ec700693e80b309164c040580bdca8c
Roque Lico Barrios <roque.lico@sisorg.com.ar>
2021-06-12 02:15:28 -0300

    se cambio el transport de los mails al de google

---
+++ config/googleapis.js
@@ -0,0 +1,8 @@
+module.exports = {
+    
+CLIENT_ID: '921614381202-vhiulkvb79umsh2vbggnr2h51u2kh9p3.apps.googleusercontent.com',
+CLIENT_SECRET:'ilObpSqfmwX-E4loJzYCrCrT',
+REDIRECT_URI: 'https://developers.google.com/oauthplayground',
+REFRESH_TOKEN: '1//04C1lZqqRRL0JCgYIARAAGAQSNwF-L9IrW8wbQQWDH-rGYD5ZO8ezA-6nDNsOG72qvojhIZapkc3uE8Beqv77b8FDJQHx_bus0sA'
+
+}
--- handlers/email.js
+++ handlers/email.js
@@ -4,15 +4,37 @@ const juice = require('juice');
 const htmlToText = require('html-to-text');
 const util = require('util');
 const emailConfig = require('../config/email');
+const googleApi = require('../config/googleapis');
+const {google} = require('googleapis');
 
-let transport = nodemailer.createTransport({
-    host: emailConfig.host,
-    port: emailConfig.port,
-    auth: {
-      user: emailConfig.user, 
-      pass: emailConfig.pass
-    }
-});
+
+const oAuth2Client = new google.auth.OAuth2 (googleApi.CLIENT_ID,googleApi.CLIENT_SECRET,googleApi.REDIRECT_URI);
+oAuth2Client.setCredentials({ refresh_token: googleApi.REFRESH_TOKEN})
+
+
+        const accesToken =  oAuth2Client.getAccessToken()
+
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth:{
+                type: 'oauth2',
+                user: 'roque.licobarrios@gmail.com',
+                clientId: googleApi.CLIENT_ID,
+                clientSecret: googleApi.CLIENT_SECRET,
+                refreshToken: googleApi.REFRESH_TOKEN,
+                accessToken: accesToken
+            }
+        })
+
+
+// let transport = nodemailer.createTransport({
+//     host: emailConfig.host,
+//     port: emailConfig.port,
+//     auth: {
+//       user: emailConfig.user, 
+//       pass: emailConfig.pass
+//     }
+// });
 
 // generar HTML
 const generarHTML = (archivo, opciones = {}) => {
@@ -23,7 +45,7 @@ exports.enviar = async (opciones) => {
     const html = generarHTML(opciones.archivo, opciones );
     const text = htmlToText.fromString(html);
     let opcionesEmail = {
-        from: 'UpTask <no-reply@uptask.com>',
+        from: '<UpTask <no-reply@uptask.com>',
         to: opciones.usuario.email, 
         subject: opciones.subject,
         text, 
